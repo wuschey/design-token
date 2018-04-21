75229849aed10ab3ccdfe2389d2cea8027230707
aminata sidibe <aminata sidibe>
2018-04-21 09:34:53 +0200

    renamed es6 file

--- output/variables.js
+++ output/variables.js
@@ -1,8 +1,8 @@
 /* SourceCode Doc/ Info */
-exports.color_white = #FFF;
-exports.color_black = #000;
-exports.color_neutral_20 = #22222;
-exports.color_neutral_90 = #EEEEE;
+exports.color_white = #FFFFFF;
+exports.color_black = #000000;
+exports.color_neutral_20 = #222222;
+exports.color_neutral_90 = #EEEEEE;
 exports.color_blue_50 = #2196F3;
 exports.color_blue_60 = #1E88E5;
 
@@ -11,5 +11,5 @@ exports.size_medium = 10px;;
 
 exports.background_color_default = #2196F3;
 
-exports.text_color_default = #FFF;
+exports.text_color_default = #FFFFFF;
 
