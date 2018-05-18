b217b9efcac0cd72473d184ad4149c1657dee86c
Aminata <aminata.sidibe@nexum.de>
2018-05-18 15:20:06 +0200

    Update

--- .es6.js
+++
@@ -1,8 +0,0 @@
-export const color_background = '#000000';
-
-export const family_heading = 'Salesforce Sans', Arial, sans-serif';
-export const family_text = 'Salesforce Sans', Arial, sans-serif';
-export const weight_bold = '700';
-
-export const text_brand = '#1589ee';
-
